c8a9f35d1c397f89a61f06c6956b9fd70feb6b86
philipseo <hbnb7894@gmail.com>
2024-09-23 01:23:27 +0900

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# purge-deps
+
+`purge-deps` is a command-line tool designed to delete all files and folders related to dependencies in JavaScript-based monorepo projects. It provides a convenient way to clean up generated files created by package managers.
+
+## Features
+
+- By default, it targets files and folders related to JavaScript package managers.
+- If you're not working with JavaScript, you can specify custom targets using the `overwrite` option.
+
+## Installation
+
+You can install `purge-deps` globally using npm:
+
+```bash
+npm install -g purge-deps
+```
+
+## Usage
+
+You can run the tool using:
+
+```bash
+npx purge
+```
+
+## Options
+
+- -h, help: Show help information.
+- -p, path <path>: Specify the path to delete files and folders. Default is the current directory (.).
+- -e, extends <targets>: Add to the list of targets to delete.
+- -o, overwrite <targets>: Replace the list of targets to delete.
+
+## Default Targets
+The default targets include:
+
+- node-modules
+- pnpm-lock.yaml
+- yarn.lock
+- package-lock.json
+
+## Example
+
+To use purge-deps:
+
+```bash
+npx purge-deps -p /path/to/your/project -e additional-file.txt
+```
---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::sync::RwLock;
+use once_cell::sync::Lazy;    
+
+static TARGETS: Lazy<RwLock<Vec<String>>> = Lazy::new(|| RwLock::new(vec![
+    "node_modules".to_string(),
+    "pnpm-lock.yaml".to_string(),
+    "yarn.lock".to_string(),
+    "package-lock.json".to_string(),
+]));
+
+fn print_help() {
+    println!("Usage: puge-deps [options]");
+    println!("Options:");
+    println!("  -h or help                  Show this help message.");
+    println!("  -p or path <path>           Specify the path to delete files and folders.");
+    println!("  -e or extends <targets>     Add to the list of targets to delete.");
+    println!("  -o or overwrite <list>      Replace the list of targets to delete.");
+}
+
+fn parse_targets(input: &str) -> Vec<String> {
+    input.split(',')
+        .filter_map(|s| {
+            let trimmed = s.trim().to_string();
+            if !trimmed.is_empty() {
+                Some(trimmed)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+fn recursive_delete_files_and_folders<P: AsRef<Path>>(
+    path: P
+) -> Result<(), std::io::Error> {
+    let entries: fs::ReadDir = fs::read_dir(path)?;
+
+    for entry in entries {
+        let entry = entry?;
+        let entry_path = entry.path();
+        let file_name_osstr = entry.file_name();
+
+        if let Some(file_name) = file_name_osstr.to_str() {
+            
+            let is_file = entry_path.is_file();
+            let is_dir = entry_path.is_dir();
+
+            if TARGETS.read().unwrap().contains(&file_name.to_string()) {
+                if is_file {
+                    println!("Deleting file: {:?}", file_name);
+                    fs::remove_file(&entry_path).map_err(|err| {
+                        eprintln!("Error deleting file {:?}: {}", entry_path, err);
+                        err
+                    })?;
+                } else if is_dir {
+                    println!("Deleting folder: {:?}", file_name);
+                    fs::remove_dir_all(&entry_path).map_err(|err| {
+                        eprintln!("Error Deleting folder {:?}: {}", entry_path, err);
+                        err
+                    })?;
+                }
+            }
+
+            if is_dir {
+                recursive_delete_files_and_folders(&entry_path)?;
+            }
+        } else {
+            eprintln!("Error: Invalid file name for entry: {:?}", entry_path);
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut path = ".";
+    let mut set_extends = false;
+    let mut set_overwrite = false;
+    
+    let mut i = 1;
+    while i < args.len() {
+        match args[i].as_str() {
+            "help" | "-h" => {
+                print_help();
+                return;
+            }
+            "path" | "-p" => {
+                if i + 1 < args.len() {
+                    path = &args[i + 1];
+                    i += 1;
+                } else {
+                    eprintln!("Error: You must specify a path after 'path'.");
+                    return;
+                }
+            }
+            "extends" | "-e" => {
+                if set_overwrite {
+                    eprintln!("Error: 'extends' cacnnot be used with 'overwrite'.");
+                    return;
+                }
+
+                if i + 1 < args.len() {
+                    let new_targets = parse_targets(&args[i + 1]);
+                    TARGETS.write().unwrap().extend(new_targets);
+                    set_extends = true;
+                    i += 1;
+                } else {
+                    eprintln!("Error: You must specify a list after 'extends'.");
+                }
+            }
+            "overwrite" | "-o" => {
+                if set_extends {
+                    eprintln!("Error: 'overwrite' cannot be used with 'extends'.");
+                    return;
+                }
+
+                if i + 1 < args.len() {
+                    let new_targets = parse_targets(&args[i + 1]);
+                    *TARGETS.write().unwrap() = new_targets;
+                    set_overwrite = true;
+                    i += 1;
+                } else {
+                    eprintln!("Error: You must specify a list after 'overwrite'.");
+                    return;
+                }
+            }
+            _ => {
+                eprintln!("Error: Unknown option {}. Use help or -h for usage information.", args[i]);
+                return;
+            }
+        }
+        i += 1;
+    }
+
+    println!("Path: {}", path);
+    println!("Targets: {:?}", *TARGETS.read().unwrap());
+
+    if let Err(err) = recursive_delete_files_and_folders(path) {
+        eprintln!("Error: {}", err);
+    }
+}
